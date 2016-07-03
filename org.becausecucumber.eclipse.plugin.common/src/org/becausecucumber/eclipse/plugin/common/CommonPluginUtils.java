fe3fad367316f82dc14a796d6160214ac708aa2a
alterhu2020@gmail.com <alterhu2020@gmail.com>
2016-07-03 08:42:21 +0800

    update the templates.xml

--- org.becausecucumber.eclipse.plugin.common/src/org/becausecucumber/eclipse/plugin/common/CommonPluginUtils.java
+++ org.becausecucumber.eclipse.plugin.common/src/org/becausecucumber/eclipse/plugin/common/CommonPluginUtils.java
@@ -97,8 +97,7 @@ public class CommonPluginUtils {
 
 		IEditorInput input = editor.getEditorInput();
 
-		IFile path = input instanceof FileEditorInput ? ((FileEditorInput) input)
-				.getFile() : null;
+		IFile path = input instanceof FileEditorInput ? ((FileEditorInput) input).getFile() : null;
 
 		return path;
 	}
@@ -129,12 +128,10 @@ public class CommonPluginUtils {
 		return resource;
 	}
 
-	public static IPath getEditorHandler(ExecutionEvent event)
-			throws ExecutionException {
+	public static IPath getEditorHandler(ExecutionEvent event) throws ExecutionException {
 		// Shell shell = HandlerUtil.getActiveShell(event);
 
-		IWorkbenchWindow windows = HandlerUtil
-				.getActiveWorkbenchWindowChecked(event);
+		IWorkbenchWindow windows = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		IEditorPart activeEditor = windows.getActivePage().getActiveEditor();
 
 		if (!(activeEditor instanceof AbstractTextEditor)) {
@@ -144,16 +141,13 @@ public class CommonPluginUtils {
 
 		IEditorInput input = editor.getEditorInput();
 
-		IPath path = input instanceof FileEditorInput ? ((FileEditorInput) input)
-				.getPath() : null;
+		IPath path = input instanceof FileEditorInput ? ((FileEditorInput) input).getPath() : null;
 		return path;
 	}
 
-	public static Object getTreeExplorerHandler(ExecutionEvent event)
-			throws ExecutionException {
+	public static Object getTreeExplorerHandler(ExecutionEvent event) throws ExecutionException {
 		Object element = null;
-		ISelectionService selection = HandlerUtil
-				.getActiveWorkbenchWindowChecked(event).getSelectionService();
+		ISelectionService selection = HandlerUtil.getActiveWorkbenchWindowChecked(event).getSelectionService();
 		if (selection instanceof IStructuredSelection) {
 			element = ((IStructuredSelection) selection).getFirstElement();
 		}
@@ -172,41 +166,41 @@ public class CommonPluginUtils {
 
 	public static IStructuredSelection getActiveIStructuredSelection() {
 
-		ISelectionService selectionService = getActiveWindow()
-				.getSelectionService();
-		IStructuredSelection selection = (selectionService instanceof IStructuredSelection) ? (IStructuredSelection) selectionService
-				: null;
+		ISelectionService selectionService = getActiveWindow().getSelectionService();
+		IStructuredSelection selection = (selectionService instanceof IStructuredSelection)
+				? (IStructuredSelection) selectionService : null;
 		return selection;
 	}
 
 	public static IProject getActiveProject() {
-		IWorkbenchWindow activeWorkbenchWindow = getActiveWindow();
-		IWorkbenchPage activePage = getActivePage();
-		ISelectionService selectionService = activeWorkbenchWindow
-				.getSelectionService();
-		IEditorPart activeEditor = activePage.getActiveEditor();
 		IProject project = null;
-		if (activeEditor != null) {
-			IFileEditorInput input = (IFileEditorInput) activeEditor
-					.getEditorInput();
-			project = input.getFile().getProject();
-		}
+		try {
+			IWorkbenchWindow activeWorkbenchWindow = getActiveWindow();
+			IWorkbenchPage activePage = getActivePage();
+			ISelectionService selectionService = activeWorkbenchWindow.getSelectionService();
+			IEditorPart activeEditor = activePage.getActiveEditor();
 
-		if (selectionService instanceof IStructuredSelection) {
-			Object element = ((IStructuredSelection) selectionService)
-					.getFirstElement();
-
-			if (element instanceof IResource) {
-				project = ((IResource) element).getProject();
-			} else if (element instanceof PackageFragmentRootContainer) {
-				IJavaProject jProject = ((PackageFragmentRootContainer) element)
-						.getJavaProject();
-				project = jProject.getProject();
-			} else if (element instanceof IJavaElement) {
-				IJavaProject jProject = ((IJavaElement) element)
-						.getJavaProject();
-				project = jProject.getProject();
+			if (activeEditor != null) {
+				IFileEditorInput input = (IFileEditorInput) activeEditor.getEditorInput();
+				project = input.getFile().getProject();
 			}
+
+			if (selectionService instanceof IStructuredSelection) {
+				Object element = ((IStructuredSelection) selectionService).getFirstElement();
+
+				if (element instanceof IResource) {
+					project = ((IResource) element).getProject();
+				} else if (element instanceof PackageFragmentRootContainer) {
+					IJavaProject jProject = ((PackageFragmentRootContainer) element).getJavaProject();
+					project = jProject.getProject();
+				} else if (element instanceof IJavaElement) {
+					IJavaProject jProject = ((IJavaElement) element).getJavaProject();
+					project = jProject.getProject();
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			Log.error("getActiveProject method exception,please make current project active,exception log: " + e);
 		}
 		return project;
 	}
@@ -215,29 +209,24 @@ public class CommonPluginUtils {
 		boolean isjava = false;
 		IWorkbenchWindow activeWorkbenchWindow = getActiveWindow();
 		IWorkbenchPage activePage = getActivePage();
-		ISelectionService selectionService = activeWorkbenchWindow
-				.getSelectionService();
+		ISelectionService selectionService = activeWorkbenchWindow.getSelectionService();
 		IEditorPart activeEditor = activePage.getActiveEditor();
 		IProject project = null;
 		if (activeEditor != null) {
-			IFileEditorInput input = (IFileEditorInput) activeEditor
-					.getEditorInput();
+			IFileEditorInput input = (IFileEditorInput) activeEditor.getEditorInput();
 			project = input.getFile().getProject();
 		}
 
 		if (selectionService instanceof IStructuredSelection) {
-			Object element = ((IStructuredSelection) selectionService)
-					.getFirstElement();
+			Object element = ((IStructuredSelection) selectionService).getFirstElement();
 
 			if (element instanceof IResource) {
 				project = ((IResource) element).getProject();
 			} else if (element instanceof PackageFragmentRootContainer) {
-				IJavaProject jProject = ((PackageFragmentRootContainer) element)
-						.getJavaProject();
+				IJavaProject jProject = ((PackageFragmentRootContainer) element).getJavaProject();
 				project = jProject.getProject();
 			} else if (element instanceof IJavaElement) {
-				IJavaProject jProject = ((IJavaElement) element)
-						.getJavaProject();
+				IJavaProject jProject = ((IJavaElement) element).getJavaProject();
 				project = jProject.getProject();
 			}
 		}
@@ -266,37 +255,30 @@ public class CommonPluginUtils {
 		return JavaCore.create(project);
 	}
 
-	public static boolean isJavaNatureProject(ExecutionEvent event)
-			throws ExecutionException {
+	public static boolean isJavaNatureProject(ExecutionEvent event) throws ExecutionException {
 		boolean isjava = false;
-		IWorkbenchPage activePage = HandlerUtil
-				.getActiveWorkbenchWindowChecked(event).getActivePage();
+		IWorkbenchPage activePage = HandlerUtil.getActiveWorkbenchWindowChecked(event).getActivePage();
 		IEditorPart activeEditor = activePage.getActiveEditor();
-		ISelectionService selectionService = HandlerUtil
-				.getActiveWorkbenchWindowChecked(event).getSelectionService();
+		ISelectionService selectionService = HandlerUtil.getActiveWorkbenchWindowChecked(event).getSelectionService();
 		ISelection selection = selectionService.getSelection();
 
 		IProject project = null;
 		if (activeEditor != null) {
-			IFileEditorInput input = (IFileEditorInput) activeEditor
-					.getEditorInput();
+			IFileEditorInput input = (IFileEditorInput) activeEditor.getEditorInput();
 			project = input.getFile().getProject();
 		}
 
 		if (selection instanceof IStructuredSelection) {
-			Object element = ((IStructuredSelection) selection)
-					.getFirstElement();
+			Object element = ((IStructuredSelection) selection).getFirstElement();
 
 			if (element instanceof IResource) {
 				project = ((IResource) element).getProject();
 			} else if (element instanceof PackageFragmentRootContainer) {
 
-				IJavaProject jProject = ((PackageFragmentRootContainer) element)
-						.getJavaProject();
+				IJavaProject jProject = ((PackageFragmentRootContainer) element).getJavaProject();
 				project = jProject.getProject();
 			} else if (element instanceof IJavaElement) {
-				IJavaProject jProject = ((IJavaElement) element)
-						.getJavaProject();
+				IJavaProject jProject = ((IJavaElement) element).getJavaProject();
 				project = jProject.getProject();
 			}
 		}
@@ -320,19 +302,17 @@ public class CommonPluginUtils {
 	}
 
 	/*
-	 * https://dev.vaadin.com/svn/integration/eclipse/plugins/com.vaadin.integration
-	 * .
+	 * https://dev.vaadin.com/svn/integration/eclipse/plugins/com.vaadin.
+	 * integration .
 	 * eclipse/src/com/vaadin/integration/eclipse/wizards/DirectoryPackageWizard
 	 * .java
 	 */
 
 	public static IStructuredSelection getValidSelection() {
-		ISelection currentSelection = JavaPlugin.getActiveWorkbenchWindow()
-				.getSelectionService().getSelection();
+		ISelection currentSelection = JavaPlugin.getActiveWorkbenchWindow().getSelectionService().getSelection();
 		if (currentSelection instanceof IStructuredSelection) {
 			IStructuredSelection structuredSelection = (IStructuredSelection) currentSelection;
-			List<IJavaProject> selectedElements = new ArrayList<IJavaProject>(
-					structuredSelection.size());
+			List<IJavaProject> selectedElements = new ArrayList<IJavaProject>(structuredSelection.size());
 			@SuppressWarnings("unchecked")
 			Iterator<Object> iter = structuredSelection.iterator();
 			while (iter.hasNext()) {
@@ -340,11 +320,9 @@ public class CommonPluginUtils {
 				if (selectedElement instanceof IProject) {
 					addProject(selectedElements, (IProject) selectedElement);
 				} else if (selectedElement instanceof IResource) {
-					addProject(selectedElements,
-							((IResource) selectedElement).getProject());
+					addProject(selectedElements, ((IResource) selectedElement).getProject());
 				} else if (selectedElement instanceof IJavaElement) {
-					addJavaElement(selectedElements,
-							(IJavaElement) selectedElement);
+					addJavaElement(selectedElements, (IJavaElement) selectedElement);
 				}
 			}
 			return new StructuredSelection(selectedElements);
@@ -354,9 +332,7 @@ public class CommonPluginUtils {
 	}
 
 	@SuppressWarnings("unchecked")
-	private static void addProject(
-			@SuppressWarnings("rawtypes") List selectedElements,
-			IProject project) {
+	private static void addProject(@SuppressWarnings("rawtypes") List selectedElements, IProject project) {
 		try {
 			if (project.hasNature(JavaCore.NATURE_ID))
 				selectedElements.add(JavaCore.create(project));
@@ -366,8 +342,7 @@ public class CommonPluginUtils {
 	}
 
 	@SuppressWarnings("unchecked")
-	public static void addJavaElement(
-			@SuppressWarnings("rawtypes") List selectedElements, IJavaElement je) {
+	public static void addJavaElement(@SuppressWarnings("rawtypes") List selectedElements, IJavaElement je) {
 		if (je.getElementType() == IJavaElement.COMPILATION_UNIT)
 			selectedElements.add(je);
 		else if (je.getElementType() == IJavaElement.CLASS_FILE)
@@ -383,25 +358,21 @@ public class CommonPluginUtils {
 		} else {
 			IOpenable openable = je.getOpenable();
 			if (openable instanceof ICompilationUnit)
-				selectedElements
-						.add(((ICompilationUnit) openable).getPrimary());
-			else if (openable instanceof IClassFile
-					&& !isInArchiveOrExternal(je))
+				selectedElements.add(((ICompilationUnit) openable).getPrimary());
+			else if (openable instanceof IClassFile && !isInArchiveOrExternal(je))
 				selectedElements.add(openable);
 		}
 	}
 
 	private static boolean isInArchiveOrExternal(IJavaElement element) {
-		IPackageFragmentRoot root = JavaModelUtil
-				.getPackageFragmentRoot(element);
+		IPackageFragmentRoot root = JavaModelUtil.getPackageFragmentRoot(element);
 		return root != null && (root.isArchive() || root.isExternal());
 	}
 
 	public static ArrayList<IResource> getAllFilesInProject() {
 		ArrayList<IResource> allCFiles = new ArrayList<IResource>();
 
-		IWorkspaceRoot myWorkspaceRoot = ResourcesPlugin.getWorkspace()
-				.getRoot();
+		IWorkspaceRoot myWorkspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
 		IProject project = CommonPluginUtils.getActiveProject();
 		IPath roots = null;
 
@@ -419,8 +390,7 @@ public class CommonPluginUtils {
 		return allCFiles;
 	}
 
-	private static void recursiveFindFiles(ArrayList<IResource> allCFiles,
-			IPath path, IWorkspaceRoot myWorkspaceRoot) {
+	private static void recursiveFindFiles(ArrayList<IResource> allCFiles, IPath path, IWorkspaceRoot myWorkspaceRoot) {
 
 		IContainer container = myWorkspaceRoot.getContainerForLocation(path);
 		// if the project is open and accessible
@@ -437,8 +407,7 @@ public class CommonPluginUtils {
 					} else {
 						fileextension = "^*.*steps.rb$";
 					}
-					Pattern compile = Pattern.compile(fileextension,
-							Pattern.CASE_INSENSITIVE);
+					Pattern compile = Pattern.compile(fileextension, Pattern.CASE_INSENSITIVE);
 					boolean found = compile.matcher(iR.getName()).find();
 					if (found)
 						allCFiles.add(iR);
@@ -498,8 +467,7 @@ public class CommonPluginUtils {
 				rubyfile = envs[k] + File.separator + "rubyw.exe";
 				File ruby = new File(rubyfile);
 				if (ruby.exists()) {
-					String cucumber = ruby.getParent() + File.separator
-							+ "cucumber";
+					String cucumber = ruby.getParent() + File.separator + "cucumber";
 					if (new File(cucumber).exists()) {
 						cucumberpath = cucumber;
 					}
@@ -521,9 +489,9 @@ public class CommonPluginUtils {
 			gempath = CommandUtils.newExecuteCommand(null, command);
 			// CommandUtils.waitForProcess(CommandUtils.p, 25000, true);
 			boolean exists = new File(gempath).exists();
-			if(exists){
-			gempath = new File(new File(gempath).getParent()).getParent()
-					+ File.separator + "bin" + File.separator + "rdebug-ide";
+			if (exists) {
+				gempath = new File(new File(gempath).getParent()).getParent() + File.separator + "bin" + File.separator
+						+ "rdebug-ide";
 			}
 			// System.out.println(file);
 		} else {
@@ -534,4 +502,3 @@ public class CommonPluginUtils {
 	}
 
 }
-
